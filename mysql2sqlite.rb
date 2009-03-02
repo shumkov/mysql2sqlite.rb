8ac683d9afecd1f35e82c7c05bbed449de252aa0
Chris Cummer <chris@postal-code.com>
2009-03-02 04:56:26 -0800

    Refactored some of the main class to compartmentalize

--- mysql2sqlite.rb
+++ mysql2sqlite.rb
@@ -11,63 +11,93 @@ class MySQL2SqliteConverter
     @database_name = database_name
     @username = username
     @password = password
-    @rm_delay = 10
+    @rm_delay = 3
     
     @output_file = @database_name + ".sql"
-    @sqlite_file = @database_name + ".sqlite"
-  end
-  
-  
-  def handle_existing_file()
-    # TODO: Replace this with a query to the user, defaulting to Y
-    if ( File.exists?( @output_file ) )
-      (1..@rm_delay).each do |count|
-        puts "SQL file #{@output_file} already exists and will be over-written in  #{@rm_delay - count} seconds. Press ctl-C to Quit"
-        sleep 1
-      end
-      
-      FileUtils.rm( @output_file ) 
-    end
+    @sqlite_database = @database_name + ".sqlite"
   end
   
   
   def mysql_to_sqlite()
+    handle_existing_files()
+    
     arr = Array[]
 
     IO.popen( "mysqldump -u #{@username} -p#{@password} --compact --compatible=ansi --complete-insert --skip-extended-insert --default-character-set=binary #{@database_name}" ) do |pipe|
       pipe.each_line do |line|
-        # Don't attempt to include lines of MySQL data that Sqlite doesn't recognize
-        next if line.include?( 'KEY "' )
-        next if line.include?( 'UNIQUE KEY ' )
-        next if line.include?( 'PRIMARY KEY ' )
+        next if contains_disallowed_sql( line )
         
-        # Replace the MySQL terms to Sqlite-friendly terms
-        line.gsub!( /unsigned /, '' )
-        line.gsub!( /auto_increment/, ' primary key' )
-        line.gsub!( /smallint\([0-9]*\)/, 'integer' )
-        line.gsub!( /tinyint\([0-9]*\)/, 'integer' )
-        line.gsub!( /int\([0-9]*\)/, 'integer' )
-        line.gsub!( /character set [^ ]*/, '' )
-        line.gsub!( /enum\([^)]*\)/, 'varchar(255)' )
-        line.gsub!( /on update [^,]*/, '' )
-        
-        # Replace other syntactic differences
-        line.gsub!( /\`/, '"' )
-        line.gsub!( /"/, '' )
-        line.gsub!( /\\'/, '\'\'' )
+        line = translate_sql_differences( line )
+        line = translate_character_differences( line )
        
         arr << line
       end 
     end
     
-    full = arr.join( '' )
-    full.gsub!( /,\n\);/, "\);\n" )
+    @line_count = arr.length
+    
+    complete_str = arr.join( '' )
+    complete_str.gsub!( /,\n\);/, "\);\n" )
     
     puts "Writing out to: #{@output_file}"
-    File.open( @output_file, 'w') { |f| f.write( full ) }
+    File.open( @output_file, 'w') { |f| f.write( complete_str ) }
     
-    puts "Pushing to Sqlite"
+    puts "Writing out to: #{@sqlite_database}"
+    return system( "cat #{@output_file} | sqlite3 #{@sqlite_database}" )
+  end
+  
+private 
+
+
+  def handle_existing_files()
+    handle_existing_file( @output_file )
+    handle_existing_file( @sqlite_database )
+  end
+  
+  
+  def handle_existing_file( file )
+    # TODO: Replace this with a query to the user, defaulting to Y
+    if ( File.exists?( file ) )
+      (1..@rm_delay).each do |count|
+        puts "WARNING: File #{file} already exists and will be over-written in  #{@rm_delay - count} seconds. Press ctl-C to Quit"
+        sleep 1
+      end
+      
+      FileUtils.rm( file ) 
+    end
+  end
+  
+  
+  # Don't attempt to include lines of MySQL data that Sqlite doesn't recognize
+  def contains_disallowed_sql( line )
+    return true if line.include?( 'KEY "' )
+    return true if line.include?( 'UNIQUE KEY ' )
+    return true if line.include?( 'PRIMARY KEY ' )
+    return false
+  end
+  
+  
+  # Replaces the MySQL terms with Sqlite-friendly terms
+  def translate_sql_differences( line )
+    line.gsub!( /unsigned /, '' )
+    line.gsub!( /auto_increment/, ' primary key' )
+    line.gsub!( /smallint\([0-9]*\)/, 'integer' )
+    line.gsub!( /tinyint\([0-9]*\)/, 'integer' )
+    line.gsub!( /int\([0-9]*\)/, 'integer' )
+    line.gsub!( /character set [^ ]*/, '' )
+    line.gsub!( /enum\([^)]*\)/, 'varchar(255)' )
+    line.gsub!( /on update [^,]*/, '' )
     
+    return line
+  end
+  
+  
+  # Replace other syntactic differences
+  def translate_character_differences( line )
+    line.gsub!( /\`/, '"' )
+    line.gsub!( /\\'/, '\'\'' )
+    
+    return line
   end
 end
 
@@ -79,13 +109,13 @@ if __FILE__ == $0
   # Requires: database name, database user and password
   if ( ARGV.length == 0 )
     puts "Usage: $0 <database name> $1 <database user> $2 <database password>"
-    exit 0
+    exit
   end
 
   conv = MySQL2SqliteConverter.new( ARGV[0], ARGV[1], ARGV[2] )
-
-  conv.handle_existing_file()
   result = conv.mysql_to_sqlite()
   
-  exit 0
+  puts ( result ) ? "Done" : "Output failed"
+  
+  exit
 end
