452b3f0b0e8d3db67ee64e281efb78880aaa8cef
Ivan Shumkov <ivan@shumkov.ru>
2010-03-25 17:35:01 +0300

    refactor initialize and configuration add mysqldump option password is optional for command line

--- mysql2sqlite.rb
+++ mysql2sqlite.rb
@@ -8,25 +8,31 @@ require 'yaml'
 
 
 class MySQL2SqliteConverter
-  def initialize( args )
-    init_result = ( args.length == 1 ) ? init_from_yaml( args ) : init_from_command_line( args )
+  
+  def initialize(args)
+    @config = {
+      :database  => nil,
+      :username  => nil,
+      :password  => nil,
+      :overwrite => true,
+      :tables    => nil,
+      :mysqldump => 'mysqldump',
+    }
 
-    puts ">> #{@password.nil?}"
+    init_result = (args.length == 1) ? init_from_yaml(args) : init_from_command_line(args)
 
-    if ( init_result && @database_name && @username )
+    if (init_result && @config[:database] && @config[:username])
       @file_deletion_delay = 10
-      @output_file, @sqlite_database = @database_name + ".sql", @database_name + ".sqlite"
+      @output_file, @sqlite_database = @config[:database] + ".sql", @config[:database] + ".sqlite"
     else
-      puts "ERROR: Failed to initialize MySQL2SqliteConverter." 
-      exit
+      raise 'Invalid configuration'
     end
   end
-  
-  
+
   def mysql_to_sqlite()
     handle_existing_files()
     
-    arr = Array[]
+    arr = []
 
     mysqldump_str = generate_mysqldump_str()
 
@@ -40,56 +46,59 @@ class MySQL2SqliteConverter
         arr << line
       end 
     end
-    
-    @line_count = arr.length
-    
-    complete_str = arr.join( '' )
-    complete_str.gsub!( /,\n\);/, "\);\n" )
-    
-    puts "Writing out to: #{@output_file}"
-    File.open( @output_file, 'w') { |f| f.write( complete_str ) }
-    
-    puts "Writing out to: #{@sqlite_database}"
-    return system( "cat #{@output_file} | sqlite3 #{@sqlite_database}" )
-  end
-  
-private 
 
-  
-  def init_from_yaml( args )
-    if ( File.exists?( args[0] ) )
-      ruby_obj = YAML::load_file( args[0] )
-      config = ruby_obj[ 'config' ]
-      
-      return ( !config.nil? ) ? init( config['database'], config['username'], config['password'], config['overwrite'], config['tables'] ) : false
-    else
-      return false
+    unless arr.empty?
+      @line_count = arr.length
+
+      complete_str = arr.join( '' )
+      complete_str.gsub!( /,\n\);/, "\);\n" )
+
+      puts "Writing out to: #{@output_file}"
+      File.open( @output_file, 'w') { |f| f.write( complete_str ) }
+
+      puts "Writing out to: #{@sqlite_database}"
+      return system( "cat #{@output_file} | sqlite3 #{@sqlite_database}" )
     end
   end
-  
-  
-  def init_from_command_line( args )
-    overwrite = ( args[3] && ( "1" == args[3] || "true" == args[3] ) ) ? true : false
-    return ( 3 <= args.length ) ? init( args[0], args[1], args[2], overwrite ) : false
-  end
-  
-  
-  def init( database_name, username, password, overwrite_files = true, tables = nil )
-    @database_name, @username, @password, @overwrite_files, @tables = database_name, username, password, overwrite_files, tables
-    return true
+
+private
+
+  def init_from_yaml(args)
+    if (File.exists?(args[0]))
+      config = YAML::load_file(args[0])
+      # Backward compatibility with old version of configs
+      config = config['config'] if config['config']
+
+      config.each do |key, value|
+        key = key.to_sym
+        if @config.has_key? key
+          @config[key] = value 
+        else
+          raise 'Invalid option'
+        end
+      end
+      true
+    end
   end
 
+  def init_from_command_line( args )
+    if (args.length >= 2)
+      @config[:database], @config[:username],  = args[0], args[1]
+      @config[:password] = args[2] if args[2]
+      @config[:overwrite] = args[3] if args[3] && ("1" == args[3] || "true" == args[3])
+      true
+    end
+  end
 
   def handle_existing_files()
     handle_existing_file( @output_file )
     handle_existing_file( @sqlite_database )
   end
   
-  
   def handle_existing_file( file )
     # TODO: Replace this with a query to the user, defaulting to Y
     if ( File.exists?( file ) )
-      if ( true != @overwrite_files )
+      if ( true != @config[:overwrite] )
         (1..@file_deletion_delay).each do |count|
           puts "WARNING: File #{file} already exists and will be over-written in  #{@file_deletion_delay - count} seconds. Press ctl-C to Quit"
           sleep 1
@@ -102,15 +111,15 @@ private
   
   
   def generate_mysqldump_str()
-    table_str = ( nil != @tables ) ? @tables.join( ' ' ) : ''
+    table_str = ( nil != @config[:tables] ) ? @config[:tables].join( ' ' ) : ''
 
-    mysqldump_str = "mysqldump5 -u #{@username} --compact --compatible=ansi --complete-insert --skip-extended-insert --default-character-set=binary #{@database_name} " + table_str
-    mysqldump_str += "-p#{@password}" if @password
-    
+    mysqldump_str = "#{@config[:mysqldump]} -u #{@config[:username]} --compact --compatible=ansi --complete-insert --skip-extended-insert --default-character-set=binary #{@config[:database]} " + table_str
+    mysqldump_str += "-p#{@config[:password]}" if @config[:password]
+
     return mysqldump_str
   end
-  
-  
+
+
   # Don't attempt to include lines of MySQL data that Sqlite doesn't recognize
   def contains_disallowed_sql( line )
     return true if line.include?( 'KEY "' )
@@ -122,25 +131,24 @@ private
   
   # Replaces the MySQL terms with Sqlite-friendly terms
   def translate_sql_differences( line )
-    line.gsub!( /unsigned /i, '' )
-    line.gsub!( /auto_increment/i, ' primary key' )
-    line.gsub!( /smallint\([0-9]*\)/i, 'integer' )
-    line.gsub!( /tinyint\([0-9]*\)/i, 'integer' )
-    line.gsub!( /int\([0-9]*\)/i, 'integer' )
-    line.gsub!( /character set [^ ]*/i, '' )
-    line.gsub!( /enum\([^)]*\)/i, 'varchar(255)' )
-    line.gsub!( /on update [^,]*/i, '' )
-    line.gsub!( /collate [^\s]+/i, '')
+    line.gsub!( /UNSIGNED /, '' )
+    line.gsub!( /AUTO_INCREMENT/, ' primary key' )
+    line.gsub!( /SMALLINT\([0-9]*\)/, 'integer' )
+    line.gsub!( /TINYINT\([0-9]*\)/, 'integer' )
+    line.gsub!( /INT\([0-9]*\)/, 'integer' )
+    line.gsub!( /CHARACTER SET [^ ]+/, '' )
+    line.gsub!( /ENUM\([^)]*\)/, 'varchar(255)' )
+    line.gsub!( /ON UPDATE [^,]*/, '' )
+    line.gsub!( /COLLATE [^\s]+/, '')
     
     return line
   end
-  
-  
+
   # Replace other syntactic differences
   def translate_character_differences( line )
     line.gsub!( /\`/, '"' )
     line.gsub!( /\\'/, '\'\'' )
-    
+
     return line
   end
 end
@@ -149,17 +157,18 @@ end
 # ------------------------------ Main ------------------------------ #
 
 
-if __FILE__ == $0
-  if ( ARGV.length == 0 )
+if __FILE__ == $0  
+  begin
+    conv = MySQL2SqliteConverter.new(ARGV)
+  rescue => e
+    puts 'ERROR: ' + e
+    puts
     puts "Usage: ./mysql2sqlite.rb database_name username password "
     puts "   or: ./mysql2sqlite.rb config_file.yaml"
-    exit
-  end
+  else
+    result = conv.mysql_to_sqlite()
 
-  conv = MySQL2SqliteConverter.new( ARGV )
-  result = conv.mysql_to_sqlite()
-  
-  puts ( result ) ? "Done" : "Output failed"
-  
+    puts ( result ) ? "Done." : "Error."
+  end
   exit
 end
